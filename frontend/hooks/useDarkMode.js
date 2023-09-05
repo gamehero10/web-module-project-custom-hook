3ea1fe8963001d9b51a05f24b8e9e4ae1629ef24
Rafael Deluna <delunarafael@yahoo.com>
2023-09-05 01:28:12 -0400

    Completed Sprint 9 Module 3 Project

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue);
+  return [darkMode, setDarkMode];
+}
+
+
+
+
+
+
+
+export default useDarkMode;
